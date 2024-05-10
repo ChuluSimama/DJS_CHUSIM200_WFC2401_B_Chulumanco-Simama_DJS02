f41836831a138be19aad0a225aa372d56ce31fa6
ChuluSimmama <phatusimama@gmail.com>
2024-05-10 17:19:45 +0200

    Dividing numbers result in decimal number resolved

--- scripts.js
+++ scripts.js
@@ -6,4 +6,12 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+
+  const divisionResult = dividend / divider;
+  if (Number.isInteger(divisionResult)) {
+    result.innerText = divisionResult;
+  } else {
+    showError("Division result is not a whole number.");
+  }
+
 });
