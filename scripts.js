2270ac73a8478607b4261f4d9bfac8e98c49baaf
ChuluSimmama <phatusimama@gmail.com>
2024-05-10 17:50:31 +0200

    parse the input values as floating point numbers.

--- scripts.js
+++ scripts.js
@@ -4,8 +4,9 @@ const result = document.querySelector("[data-result]");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  const dividend = parseFloat(entries.get("dividend"));
+  const divider = parseFloat(entries.get("divider"));
+
 
   const divisionResult = dividend / divider;
   if (Number.isInteger(divisionResult)) {
